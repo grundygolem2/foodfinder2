c4f6ce36fb4da158cd5c9f7cc9e33e665769f5f8
Perditian <david.taus@tufts.edu>
2015-10-24 08:15:30 -0400

    moddified addmarkers.js to create infowindows

--- static/js/addMarkers.js
+++ static/js/addMarkers.js
@@ -5,12 +5,25 @@ function parseData(data)
         //var locations = JSON.parse(data);
         for(var i = 0; i < data.length; i++){
                 var pos = new google.maps.LatLng(data[i].lat, data[i].lon);
-                var marker = new google.maps.Marker({
-                        position: pos,
-                        title: data[i].name + " described as " + data[i].desc
-                });
+                var descrip = data[i].name + " described as " + data[i].desc;
 
-                marker.setMap(map);
-
+                makeInfowindows(pos, descrip);
         }
+}
+
+function makeInfowindows(pos, description)
+{
+        var marker;
+        var infowindow;
+        marker = new google.maps.Marker({
+                position: pos,
+                title: description
+        });
+
+        marker.setMap(map);
+        infowindow = new google.maps.InfoWindow({content: marker.title});
+        google.maps.event.addListener(marker, 'click', function() {
+                infowindow.close();
+                infowindow.open(map, this);
+                });
 }
