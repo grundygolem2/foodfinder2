f14292dba12958288e76b584f15199dacbba7246
Perditian <david.taus@tufts.edu>
2015-10-24 07:26:25 -0400

    Fixed table auto filler, Thanks Riley for reading the code

--- static/js/populateTable.js
+++ static/js/populateTable.js
@@ -1,21 +1,24 @@
 function populateTable(data)
 {
-        var tbod = document.getElementsbyTagName("tbody");
-        for(int i = 0; i < data.length; i++){
-                trow = document.createElement("tr");
+        console.log(data);
+        var tbod = document.getElementById("tabpart");
+        console.log(data)
+        for(var i = 0; i < data.length; i++){
+                console.log(data[i]);
+                var trow = document.createElement("tr");
 
                 //Title
-                tdat = docmunt.createElement("td");
+                var tdat = document.createElement("td");
                 tdat.appendChild(document.createTextNode(data[i].name));
                 trow.appendChild(tdat);
 
                 //Description
-                tdat = docmunt.createElement("td");
+                tdat = document.createElement("td");
                 tdat.appendChild(document.createTextNode(data[i].desc));
                 trow.appendChild(tdat);
 
                 //Start Time
-                tdat = docmunt.createElement("td");
+                tdat = document.createElement("td");
                 var stime = data[i].starttime.split(" ");
                 var stimesec = stime[1].split(":");
                 var startim = stimesec[0] + ":" + stimesec[1];
@@ -23,15 +26,15 @@ function populateTable(data)
                 trow.appendChild(tdat);
 
                 //End Time
-                tdat = docmunt.createElement("td");
-                var etime = data[i].starttime.split(" ");
+                tdat = document.createElement("td");
+                var etime = data[i].endtime.split(" ");
                 var etimesec = etime[1].split(":");
                 var endtim = etimesec[0] + ":" + etimesec[1];
                 tdat.appendChild(document.createTextNode(endtim));
                 trow.appendChild(tdat);
 
                 //Distance(miles)
-                tdat = docmunt.createElement("td");
+                tdat = document.createElement("td");
                 tdat.appendChild(document.createTextNode(data[i].dist));
                 trow.appendChild(tdat);
 
